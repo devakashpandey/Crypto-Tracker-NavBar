ec8262654cddee7a162910cac29ec3fb5753cc70
DevAkky <developerakky@gmaiil.com>
2023-02-17 02:24:07 +0530

    installing axios

---
+++ src/components/Carousel.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Carousel.css";
+
+const Carousel = () => {
+
+ 
+
+  return (
+    <>
+    <div className='my-carousel'>
+        AKASH
+     </div>
+    </>
+  )
+}
+
+export default Carousel;
